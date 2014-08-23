66c8a435d8704df39e1b6bfc65fa08d320ec045e
seckcoder <seckcoder@gmail.com>
2014-08-23 12:39:58 -0700

    update

--- python/test.py
+++ python/test.py
@@ -1,15 +1,47 @@
-from xlwt import Workbook
 
-book = Workbook()
-sheet = book.add_sheet('Sheet 1')
-# row0 col0
-sheet.write(0, 0, 'A1')
-# row0 col1
-sheet.write(0, 1, 'B1')
-# get row1
-row1 = sheet.row(1)
-# row1 col0
-row1.write(0, 'A2')
-# row1 col1
-row1.write(1, 'B2')
-book.save('simple.xls')
+
+
+
+total = 0
+c_total = 0
+h_total = 0
+l_total = 0
+y_total = 0
+s_total = 0
+
+
+with open("data/ikea.txt", "r") as fin:
+    for l in fin:
+        l = l.strip()
+        if l:
+            count, ty = l.split(' ')
+            count = float(count)
+            total += count
+            if ty == "c":
+                c_total += count
+            elif ty == "h":
+                h_total += count
+            elif ty == "y":
+                y_total += count
+            elif ty == "l":
+                l_total += count
+            elif ty == "s":
+                s_total += count
+            else:
+                print "error"
+def withtax(c):
+    return c*1.09
+
+Meta = {
+    "c":c_total,
+    "h":h_total,
+    "l":l_total,
+    "y":y_total,
+    "s":s_total
+}
+
+withtax_total = 0
+for ty, cnt in Meta.iteritems():
+    print ty, cnt, withtax(cnt)
+    withtax_total += withtax(cnt)
+print withtax_total
