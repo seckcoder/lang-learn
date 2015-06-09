9d60c41a6dbf78125505abfa2418b8705d5697ba
seckcoder <seckcoder@gmail.com>
2015-06-08 21:45:32 -0700

    move leetcode

--- algo/acm/leetcode/Next Permutation/1.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    # @param num, a list of integer
-    # @return nothing (void), do not return anything, modify num in-place instead.
-    def nextPermutation(self, num):
-        
--- algo/acm/leetcode/base.h
+++
@@ -1,6 +0,0 @@
-#include <cstdlib>
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
